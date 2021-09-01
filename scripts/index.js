4f1cd2f30e02ff43069a3349fbf42bfd5c238a07
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2021-09-01 18:59:57 +0200

    Update class html

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+let bill = document.querySelector('.bill').value;
+let btnPercent = document.getElementsByClassName('btn-percent');
+let inputPercent = document.querySelector('.input-percent');
+let people = document.querySelector('.people').value;
+let btnOutput = document.querySelector('.btn-output');
+
+console.log(bill);
+// console.log(btnPercent);
+// console.log(inputPercent);
+// console.log(people);
+// console.log(btnOutput);
+
+// bill.addEventListener('input', () => {}, false);
