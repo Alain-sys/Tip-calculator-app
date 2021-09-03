13a64887437e5a2a1df32566b802541daa97bfb2
Vcna-0 <36771179+Vcna-0@users.noreply.github.com>
2021-09-03 21:39:03 +0200

    feat: js input

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,43 @@
-let bill = document.querySelector('.bill').value;
+let bill = document.querySelector('.bill');
 let btnPercent = document.getElementsByClassName('btn-percent');
 let inputPercent = document.querySelector('.input-percent');
-let people = document.querySelector('.people').value;
+let people = document.querySelector('.people');
 let btnOutput = document.querySelector('.btn-output');
+let allInput = document.querySelectorAll('input');
+let outputAccount = document.querySelector('.amount');
+let outputTotal = document.querySelector('.total');
 
-console.log(bill);
-// console.log(btnPercent);
-// console.log(inputPercent);
-// console.log(people);
-// console.log(btnOutput);
 
-// bill.addEventListener('input', () => {}, false);
+// Check input is empty and change the button-reset color 
+allInput.forEach(function(input) {
+    input.addEventListener("input", function() {
+        if ((bill.value != "") || (people.value != "") || (inputPercent.value != "")){
+            btnOutput.style.background = "#26C2AE";
+            btnOutput.style.cursor = "pointer";
+        }else{
+            btnOutput.style.background = "#0D686D";
+            btnOutput.style.cursor = "auto";
+        }
+    });
+});
+
+function calcul(e) {
+    let ValueBtn = e.target.value;
+    let tipAccount = bill.value * (ValueBtn / 100) / people.value;
+    let total = bill.value / people.value + tipAccount;
+    outputAccount.innerHTML = tipAccount;
+    outputTotal.innerHTML = total;
+}
+
+for(let i = 0; i < btnPercent.length; i++){
+    btnPercent[i].addEventListener("click", calcul)
+}
+
+inputPercent.addEventListener("input", calcul);
+inputPercent.addEventListener("click", calcul);
+
+
+            
+btnOutput.addEventListener("click", function() {
+    bill.value= "";
+})
